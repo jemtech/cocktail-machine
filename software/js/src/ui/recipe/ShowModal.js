7d6f2ca9f5231ec4ebaf20037a521726f7473a97
Jan-Erik Matthies <jem@jemtech.de>
2023-06-11 21:46:35 +0200

    broken html

--- software/js/src/ui/recipe/ShowModal.js
+++ software/js/src/ui/recipe/ShowModal.js
@@ -10,7 +10,7 @@ class ShowModal extends UiElement {
 			}
 		}
 		
-		this.baseElement = $('<div class="modal" tabindex="-1">></div>');
+		this.baseElement = $('<div class="modal" tabindex="-1"></div>');
 		super.setBaseJQueryObject(this.baseElement);
 		this.modalDialog = $('<div class="modal-dialog"></div>');
 		this.baseElement.append(this.modalDialog);
