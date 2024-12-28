80a87b21fe490b2d114b60147ce5f042fca15b32
Jan-Erik Matthies <jem@jemtech.de>
2024-12-28 11:08:22 +0100

    calibration by config

--- software/cocktailMashine.py
+++ software/cocktailMashine.py
@@ -34,7 +34,9 @@ class CocktailMashine:
                 pcf_address = int(pumpConfig['pcf_address'],0),
                 i2c_port_num = int(pumpConfig['i2c_port_num']),
                 pcf_fw = int(pumpConfig['pcf_fw']),
-                pcf_bw = int(pumpConfig['pcf_bw']))
+                pcf_bw = int(pumpConfig['pcf_bw']),
+                mlPerS = float(pumpConfig['ml_per_s'])
+            )
             self.pumps.append(pump)
             
 def getCocktailMashineSigelton():
